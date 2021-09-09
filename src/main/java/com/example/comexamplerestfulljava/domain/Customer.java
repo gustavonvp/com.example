4e23dbb1fe385f90a6639ef49814d6689b243530
gustavonvp <gustavonvp@gmail.com>
2021-09-09 17:10:10 -0300

    Adding Domain. Closes #2

--- src/main/java/com/example/comexamplerestfulljava/domain/Customer.java
+++ src/main/java/com/example/comexamplerestfulljava/domain/Customer.java
@@ -1,28 +1,12 @@
 package com.example.comexamplerestfulljava.domain;
-<<<<<<< HEAD
 
-=======
->>>>>>> eb2af30 (Adding Domain. Closes #2)
 import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-<<<<<<< HEAD
-@Data
-@Entity
-public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstname;
-    private String lastname;
-
-}
-=======
 @Entity
 @Data
 public class Customer {
@@ -34,5 +18,3 @@ public class Customer {
         private String lastname;
 
     }
-
->>>>>>> eb2af30 (Adding Domain. Closes #2)
